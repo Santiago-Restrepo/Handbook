792504364d3a96ee2a2e82186b79528ef3ab3400
Santiago Restrepo <santirstrp@gmail.com>
2022-01-28 16:25:18 -0500

    Login page structure but without styles and database connection nethier

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ export const App = () => {
                 <BrowserRouter>
                     { /** Lo que cambiará */}
                     <Switch>
-                        <Route exact path="/" component={Home} />
-                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/" component={Register} />
+                        <Route exact path="/home" component={Home} />
                         <Route exact path="/login" component={Login} />
                         <Route exact path="/profile" component={Profile} />
                         <Route exact path="/chat" component={Chat} />
